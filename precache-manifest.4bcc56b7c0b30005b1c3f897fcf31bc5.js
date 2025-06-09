2cd6b5a140c03b2c051593a6449bee62cdf860ea
Your Name <you@example.com>
2025-06-08 21:28:19 -0400

    Updates

---
+++ precache-manifest.4bcc56b7c0b30005b1c3f897fcf31bc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552fba417317459b307b8e7671b78619",
+    "url": "/coco-styling/index.html"
+  },
+  {
+    "revision": "6c24e46cc17214d38155",
+    "url": "/coco-styling/static/css/main.21ae36c4.chunk.css"
+  },
+  {
+    "revision": "4f8cf29702aa5b56c9ee",
+    "url": "/coco-styling/static/js/2.bbcc9a58.chunk.js"
+  },
+  {
+    "revision": "a70cfba45799f8a4deb5",
+    "url": "/coco-styling/static/js/3.cf2aeaea.chunk.js"
+  },
+  {
+    "revision": "6c24e46cc17214d38155",
+    "url": "/coco-styling/static/js/main.d1abfb49.chunk.js"
+  },
+  {
+    "revision": "f3aa2c475d78dedad692",
+    "url": "/coco-styling/static/js/runtime~main.20da769d.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/coco-styling/static/media/logo.06e73328.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Zahra46031/coco-styling/precache-manifest.3868936afe56e928566660602bdf82f7.js"
+  "/coco-styling/precache-manifest.4bcc56b7c0b30005b1c3f897fcf31bc5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Zahra46031/coco-styling/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coco-styling/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
