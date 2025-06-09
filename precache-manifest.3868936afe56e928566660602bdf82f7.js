4372d47e55c916fdeba33f099ed5f677e962b6e7
Your Name <you@example.com>
2025-06-08 21:13:28 -0400

    Updates

---
+++ precache-manifest.3868936afe56e928566660602bdf82f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190fd0fa75628a35b3fa72022d930d81",
+    "url": "/Zahra46031/coco-styling/index.html"
+  },
+  {
+    "revision": "6c24e46cc17214d38155",
+    "url": "/Zahra46031/coco-styling/static/css/main.21ae36c4.chunk.css"
+  },
+  {
+    "revision": "eccc57cca081a23355f8",
+    "url": "/Zahra46031/coco-styling/static/js/2.45390c38.chunk.js"
+  },
+  {
+    "revision": "a70cfba45799f8a4deb5",
+    "url": "/Zahra46031/coco-styling/static/js/3.cf2aeaea.chunk.js"
+  },
+  {
+    "revision": "6c24e46cc17214d38155",
+    "url": "/Zahra46031/coco-styling/static/js/main.d1abfb49.chunk.js"
+  },
+  {
+    "revision": "be96f5c2e75ef6ee9f70",
+    "url": "/Zahra46031/coco-styling/static/js/runtime~main.7de31b90.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Zahra46031/coco-styling/static/media/logo.06e73328.svg"
+  }
+]);
